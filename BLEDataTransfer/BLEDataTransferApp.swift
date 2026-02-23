61f318b6a032bba110d022e98053aab87c93a26d
James Afonja <97204146+jamesafonja@users.noreply.github.com>
2026-02-23 02:32:42 -0600

    Initial Commit

---
+++ BLEDataTransfer/BLEDataTransferApp.swift
@@ -0,0 +1,17 @@
+//
+//  BLEDataTransferApp.swift
+//  BLEDataTransfer
+//
+//  Created by SAfonja on 2/23/26.
+//
+
+import SwiftUI
+
+@main
+struct BLEDataTransferApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BLEDataTransferTests/BLEDataTransferTests.swift
@@ -0,0 +1,17 @@
+//
+//  BLEDataTransferTests.swift
+//  BLEDataTransferTests
+//
+//  Created by SAfonja on 2/23/26.
+//
+
+import Testing
+@testable import BLEDataTransfer
+
+struct BLEDataTransferTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
